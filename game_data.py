c86802967bfff236c8e90309babaedc363841889
OussamaZermouq <oussamazermouq2@gmail.com>
2023-07-05 00:53:53 +0100

    1st level is set

--- game_data.py
+++ game_data.py
@@ -7,3 +7,19 @@ level_0 = {
     'enemies':'Tiled/Levels/0/Level_0_enemies.csv',
     'fg_palm':'Tiled/Levels/0/Level_0_fg_palm.csv'
 }
+
+level_0_overworld = {'node_pos':(250,360),
+                     'content':'level 0',
+                     'unlock ':1}
+level_1_overworld = {'node_pos':(730,750),
+                     'content':'level 1',
+                     'unlock ':2}
+level_2_overworld = {'node_pos':(1200,300),
+                     'content':'level 2',
+                     'unlock ':2}
+
+levels = {
+    0: level_0_overworld,
+    1: level_1_overworld,
+    2: level_2_overworld,
+}
--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import pygame
 from settings import *
 from level import Level
 from game_data import level_0
+from overworld import Overworld
 
+class Game:
+    def __init__(self):
+        self.max_level =2
+        self.overworld = Overworld(2,self.max_level,screen)
+    def run(self):
+        self.overworld.run()
+        
 pygame.init()
 screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 clock=pygame.time.Clock()
 level = Level(level_0,screen)
+game = Game()
 pygame.mixer.init()
 pygame.mixer.music.load("Assets/Music/song0.mp3")
-pygame.mixer.music.play(-1)
+#pygame.mixer.music.play(-1)
+
 
 while True:
     for event in pygame.event.get():
@@ -19,5 +29,6 @@ while True:
             sys.exit()
 
     level.run()
+    #game.run()
     pygame.display.update()
     clock.tick(60)
